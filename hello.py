e2bac9a641e51ce6fc6d8f0f5006ff055ee59837
@raspiduino <68118236+raspiduino@users.noreply.github.com>
2021-01-30 11:51:47 +0700

    Upload test files
    hello.py - Simple print() test
    pygametest.py - Pygame test. Require pygame

---
+++ hello.py
@@ -0,0 +1,15 @@
+'''
+rpu - The stupid realtime Python updater
+Copyright @raspiduino 2021
+Date created 27/1/2021
+----------------------------------------
+This is the test file for rpu. You can
+modify any of this and try.
+Usage: rpu.py hello.py
+Require nothing
+
+This test file show you a hello world
+example.
+'''
+
+print("Hello world!") # You can modify this to see change.
---
+++ pygametest.py
@@ -0,0 +1,26 @@
+'''
+rpu - The stupid realtime Python updater
+Copyright @raspiduino 2021
+Date created 27/1/2021
+----------------------------------------
+This is the test file for rpu. You can
+modify any of this and try.
+Usage: rpu.py pygametest.py
+Require pygame
+
+This test file show you how to edit a
+pygame script and see the change.
+'''
+
+import pygame
+import sys
+
+pygame.init()
+
+test = pygame.display.set_mode((500, 500))
+
+test.fill([255,255,255])
+
+pygame.draw.rect(test, (15,49,86), (50, 70, 200, 100))
+
+pygame.display.update()
